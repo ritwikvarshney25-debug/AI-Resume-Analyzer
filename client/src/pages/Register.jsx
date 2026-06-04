41a431270806b0b2fd343f9bede93b14ba5a6d51
Ritwik Varshney <ritwikvarshney25@gmail.com>
2026-06-05 00:24:28 +0530

    Added register functionality

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,19 +1,68 @@
+import { useState } from "react";
+import axios from "axios";
+
 function Register() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await axios.post(
+        "https://ai-resume-analyzer-c1px.onrender.com/api/auth/register",
+        {
+          name,
+          email,
+          password,
+        }
+      );
+
+      alert(res.data.message);
+
+    } catch (error) {
+      alert(
+        error.response?.data?.message || "Registration Failed"
+      );
+    }
+  };
+
   return (
-    <div>
+    <div style={{ padding: "30px" }}>
       <h1>Register</h1>
 
-      <form>
-        <input type="text" placeholder="Enter Name" />
+      <form onSubmit={handleRegister}>
+        <input
+          type="text"
+          placeholder="Enter Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+
         <br /><br />
 
-        <input type="email" placeholder="Enter Email" />
+        <input
+          type="email"
+          placeholder="Enter Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
         <br /><br />
 
-        <input type="password" placeholder="Enter Password" />
+        <input
+          type="password"
+          placeholder="Enter Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
         <br /><br />
 
-        <button>Register</button>
+        <button type="submit">
+          Register
+        </button>
       </form>
     </div>
   );
