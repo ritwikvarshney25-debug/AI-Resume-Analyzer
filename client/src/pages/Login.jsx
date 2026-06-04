9e2a78b9750884e43e9e719fff11256136ebf01e
Ritwik Varshney <ritwikvarshney25@gmail.com>
2026-06-05 00:23:03 +0530

    Added login functionality

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,16 +1,61 @@
+import { useState } from "react";
+import axios from "axios";
+
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await axios.post(
+        "https://ai-resume-analyzer-c1px.onrender.com/api/auth/login",
+        {
+          email,
+          password,
+        }
+      );
+
+      alert(res.data.message);
+
+      localStorage.setItem("token", res.data.token);
+
+    } catch (error) {
+      alert(
+        error.response?.data?.message || "Login Failed"
+      );
+    }
+  };
+
   return (
-    <div>
+    <div style={{ padding: "30px" }}>
       <h1>AI Resume Analyzer</h1>
 
-      <form>
-        <input type="email" placeholder="Enter Email" />
-        <br /><br />
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="Enter Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
-        <input type="password" placeholder="Enter Password" />
-        <br /><br />
+        <br />
+        <br />
 
-        <button>Login</button>
+        <input
+          type="password"
+          placeholder="Enter Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <br />
+        <br />
+
+        <button type="submit">
+          Login
+        </button>
       </form>
     </div>
   );
