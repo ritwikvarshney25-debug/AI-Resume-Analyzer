88f41ab26c403582cc487dc716786aa226590a38
Ritwik Varshney <ritwikvarshney25@gmail.com>
2026-06-06 00:00:57 +0530

    Added GitHub login button

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const resumeRoutes = require("./routes/resumeRoutes");
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("./passport");
 require("dotenv").config();
 
 const authRoutes = require("./routes/authRoutes");
@@ -11,8 +13,36 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(
+  session({
+    secret: "githubloginsecret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use("/api/auth", authRoutes);
 app.use("/api/resume", resumeRoutes);
+
+app.get(
+  "/api/auth/github",
+  passport.authenticate("github", {
+    scope: ["user:email"],
+  })
+);
+
+app.get(
+  "/api/auth/github/callback",
+  passport.authenticate("github", {
+    failureRedirect: "/",
+  }),
+  (req, res) => {
+    res.redirect("https://ai-resume-analyzer-tawny-rho.vercel.app/dashboard");
+  }
+);
 
 mongoose
   .connect(process.env.MONGO_URI)
